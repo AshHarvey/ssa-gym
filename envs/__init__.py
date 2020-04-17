e35b97a951f492b259bdf2c4cca78efb705f049e
ashvoid <8992281+ashvoid@users.noreply.github.com>
2020-04-17 16:39:24 -0400

    Update __init__.py
    Removed unpublished envs

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,35 +1,7 @@
 
 from gym.envs.registration import register
 
-
-register(
-    id='ssa_tasker-v0',
-    entry_point='envs.ssa_tasker:SSA_Tasker_Env',
-)
-
-register(
-    id='ssa_tasker-v1',
-    entry_point='envs.ssa_tasker_1:SSA_Tasker_Env',
-)
-
-
-register(
-    id='ssa_tasker-v3',
-    entry_point='envs.ssa_tasker_3:SSA_Tasker_Env',
-)
-
-
-register(
-    id='ssa_tasker-v4',
-    entry_point='envs.ssa_tasker_4:SSA_Tasker_Env',
-)
-
 register(
     id='ssa_tasker_simple-v1',
     entry_point='envs.ssa_tasker_simple_1:SSA_Tasker_Env',
-)
-
-register(
-    id='oe_grid-v4',
-    entry_point='envs.oe_grid_4:EphemOEGrid4',
 )
