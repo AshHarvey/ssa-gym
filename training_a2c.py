cd1ad0354401da04a8d7dec6c9b31c8d8d326e39
ashvoid <8992281+ashvoid@users.noreply.github.com>
2020-04-18 15:43:41 -0400

    Updated to reflect 16k model

--- training_a2c.py
+++ training_a2c.py
@@ -4,9 +4,9 @@ from stable_baselines import A2C
 import envs.ssa_tasker_simple_1
 
 model = A2C('MlpPolicy', 'ssa_tasker_simple-v1', verbose=1, tensorboard_log="./a2c_ssa_50_tensorboard/")
-model.learn(total_timesteps=500000)
+model.learn(total_timesteps=16000)
 
 
-model.save("a2c_tasker_ts1m")
+model.save("a2c_tasker_ts16k")
 
 
