6bb7a4fb4b853dafded596e905eac47110121185
ash <8992281+AshHarvey@users.noreply.github.com>
2020-06-05 18:00:05 -0400

    Adds registration for new environment

--- envs/__init__.py
+++ envs/__init__.py
@@ -5,3 +5,8 @@ register(
     id='ssa_tasker_simple-v1',
     entry_point='envs.ssa_tasker_simple_1:SSA_Tasker_Env',
 )
+
+register(
+    id='ssa_tasker_simple-v2',
+    entry_point='envs.ssa_tasker_simple_2:SSA_Tasker_Env',
+)
