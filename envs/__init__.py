12fc8121adde2145355afb132b5fb60e2771f6a4
ash <8992281+AshHarvey@users.noreply.github.com>
2020-06-13 16:20:21 -0400

    Deprecated v1 of the environment

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,11 +2,6 @@
 from gym.envs.registration import register
 
 register(
-    id='ssa_tasker_simple-v1',
-    entry_point='envs.ssa_tasker_simple_1:SSA_Tasker_Env',
-)
-
-register(
     id='ssa_tasker_simple-v2',
     entry_point='envs.ssa_tasker_simple_2:SSA_Tasker_Env',
 )
