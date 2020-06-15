35c7f92cedaaa4215bce3386ab36abd8a67f7013
ash <8992281+AshHarvey@users.noreply.github.com>
2020-06-15 13:49:11 -0400

    Moved tests to main folder, updated imports on run_envs

--- run_environment.py
+++ run_environment.py
@@ -1,14 +1,15 @@
 import gym
 import numpy as np
-import envs.ssa_tasker_simple_2
 from tqdm import tqdm
 from datetime import datetime
 from envs.dynamics import fx_xyz_farnocchia, fx_xyz_markley, fx_xyz_cowell
+
 
 kwargs = {'steps': 2880, 'rso_count': 50, 'time_step': 30.0, 't_0': datetime(2020, 5, 4, 0, 0, 0),
           'obs_limit': 15, 'observer': (38.828198, -77.305352, 20.0), 'x_sigma': (1000, 1000, 1000, 10, 10, 10),
           'z_sigma': (1, 1, 1000), 'q_sigma': 0.001, 'update_interval': 1, 'orbits': np.load('envs/sample_orbits.npy'),
           'fx': fx_xyz_farnocchia}
+
 env = gym.make('ssa_tasker_simple-v2', **kwargs)
 env.seed(0)
 _ = env.reset()
