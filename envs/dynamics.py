ba84bfc434eb5b664e45a01c686452233533c01b
ash <8992281+AshHarvey@users.noreply.github.com>
2020-07-15 18:31:56 -0400

    Updated: init state sampling, hx funcs; Added: robust cholesky, mean enu/uvw; Deprecated old aer func

--- envs/dynamics.py
+++ envs/dynamics.py
@@ -4,14 +4,17 @@ from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from numba import njit, jit
 from poliastro.core.propagation import markley, vallado, pimienta, gooding, danby, farnocchia,  mikkola, func_twobody
 from poliastro.core.elements import coe2rv
-from envs.transformations import _itrs2azel
+from envs.transformations import _itrs2azel, ecef2aer, uvw2aer, aer2uvw, lla2ecef
 from poliastro.bodies import Earth
 import functools
 from astropy import _erfa as erfa
 from scipy.integrate import DOP853, solve_ivp, RK45
+from scipy.linalg import cholesky
+import pymap3d as pm
 
 k = Earth.k.to_value(u.m**3/u.s**2)
-RE = Earth.R_mean.to_value(u.m)
+RE_mean = Earth.R_mean.to_value(u.m)
+RE_eq = Earth.R.to_value(u.m)
 
 
 @njit
@@ -89,13 +92,7 @@ def fx_xyz_danby(x, dt, k=k):
 
 @njit
 def fx_xyz_farnocchia(x, dt, k=k):
-    r0 = x[:3]
-    v0 = x[3:]
-    tof = dt
-    rv = farnocchia(k, r0, v0, tof)
-    x_post = np.zeros(6)
-    x_post[:3] = rv[0]
-    x_post[3:] = rv[1]
+    x_post = np.reshape(farnocchia(k, x[:3], x[3:], dt), 6)
     return x_post
 
 
@@ -145,43 +142,37 @@ def hx_xyz(x):
     return x[:3]
 
 
-@njit
-def hx_aer_erfa(x_gcrs, trans_matrix, observer_itrs):
+def hx_aer_erfa_old(x_gcrs, trans_matrix, observer_lla, time=None, observer_itrs=None):
     # measurement function - convert state into a measurement
     # where measurements are [azimuth, elevation]
-    #trans_matrix = hx_args[0][0]
-    #observer_itrs = hx_args[0][1]
     x_itrs = trans_matrix @ x_gcrs[:3]
     aer = _itrs2azel(observer_itrs, x_itrs)
     return aer
 
 
-def hx_aer_kwargs(x_gcrs, **kwargs):
+def hx_aer_erfa(x_gcrs, trans_matrix, observer_lla, observer_itrs, time=None):
     # measurement function - convert state into a measurement
     # where measurements are [azimuth, elevation]
-    trans_matrix = kwargs["trans_matrix"]
-    observer_itrs = kwargs["observer_itrs"]
     x_itrs = trans_matrix @ x_gcrs[:3]
-    aer = _itrs2azel(observer_itrs, x_itrs)
+    aer = ecef2aer(observer_lla, x_itrs, observer_itrs)
     return aer
 
 
-def hx_aer_astropy(x, *hx_args):
+def hx_aer_astropy(x_gcrs, time, observer_lla=None, trans_matrix=None, observer_itrs=None):
     # measurement function - convert state into a measurement
     # where measurements are [azimuth, elevation]
-    t = hx_args[0][0]
-    obs_lat = hx_args[0][1]
-    obs_lon = hx_args[0][2]
-    obs_height = hx_args[0][3]
-    object = SkyCoord(x=x[0] * u.km, y=x[1] * u.km, z=x[2] * u.km, frame='gcrs',
-                   representation_type='cartesian', obstime=t)
-    obs = EarthLocation.from_geodetic(lon=obs_lon*u.rad, lat=obs_lat*u.rad, height=obs_height*u.m)
-    AltAz_frame = AltAz(obstime=t, location=obs)
+    object = SkyCoord(x=x_gcrs[0] * u.m, y=x_gcrs[1] * u.m, z=x_gcrs[2] * u.m, frame='gcrs',
+                   representation_type='cartesian', obstime=time)
+    if observer_itrs is None:
+        obs = EarthLocation.from_geodetic(lat=observer_lla[0] * u.rad, lon=observer_lla[1] * u.rad, height=observer_lla[2] * u.m)
+    else:
+        obs = EarthLocation.from_geocentric(x=observer_itrs[0] * u.m, y=observer_itrs[1] * u.m, z=observer_itrs[2] * u.m)
+    AltAz_frame = AltAz(obstime=time, location=obs)
     results = object.transform_to(AltAz_frame)
 
     az = results.az.to_value(u.rad)
     alt = results.alt.to_value(u.rad)
-    sr = results.distance.to_value(u.km)
+    sr = results.distance.to_value(u.m)
     aer = np.array([az, alt, sr])
     return aer
 
@@ -189,20 +180,10 @@ def hx_aer_astropy(x, *hx_args):
 @jit(['float64[::1](float64[::1],float64[::1])'])
 def residual_z_aer(a, b):
     # prep array to receive results
-    c = np.copy(a)
-    # force angles in range <-pi, pi>
-    while a[1] > np.pi or a[1] < -np.pi:
-        a[0] = a[0] - np.sign(a[0])*np.pi*2
-    while a[1] > np.pi or a[1] < -np.pi:
-        a[1] = a[1] - np.sign(a[1])*np.pi*2
-    # find differ, a - b
-    c[0] = (a[0] - b[0] + np.pi) % (np.pi*2) - np.pi
-    c[1] = (a[1] - b[1] + np.pi) % (np.pi*2) - np.pi
-    c[2] = np.subtract(a[2], b[2])
-    while c[0] > np.pi or c[0] < -np.pi:
-        c[0] = c[0] - np.sign(c[0]) * np.pi * 2
-    while c[1] > np.pi or c[1] < -np.pi:
-        c[1] = c[1] - np.sign(c[1]) * np.pi * 2
+    c = np.empty(a.shape)
+    c[0] = np.arctan2(np.sin(a[0]-b[0]), np.cos(a[0]-b[0])) # azimuth: input range (0, 2pi), output range (-pi, pi)
+    c[1] = (a[1] - b[1]) # elevation: input range (-pi/2, pi/2), output range (-pi, pi)
+    c[2] = (a[2] - b[2])
     return c
 
 
@@ -213,7 +194,8 @@ def residual_xyz(a, b):
 
 
 @njit
-def mean_z_aer(sigmas, Wm):
+def mean_z_aer_depro(sigmas, Wm):
+    # deprecated
     z = np.zeros(3)
     sum_sin_az, sum_cos_az, sum_sin_el, sum_cos_el = 0., 0., 0., 0.
 
@@ -261,13 +243,80 @@ def mean_z_unit_vector(sigmas, Wm):
     return z
 
 
-def init_state_vec(sma=((RE + 400000), 42164000), ecc=(0.001, 0.3), inc=(0, 180), raan=(0, 360), argp=(0, 360),
-                   nu=(0, 360), random_state=np.random.RandomState()):
-    a = random_state.uniform(sma[0], sma[1]) # (m) – Semi-major axis.
-    ecc = random_state.uniform(ecc[0], ecc[1]) # (Unit-less) – Eccentricity.
-    inc = np.radians(random_state.uniform(inc[0], inc[1])) # (rad) – Inclination
-    raan = np.radians(random_state.uniform(raan[0], raan[1])) # (rad) – Right ascension of the ascending node.
-    argp = np.radians(random_state.uniform(argp[0], argp[1])) # (rad) – Argument of the pericenter.
-    nu = np.radians(random_state.uniform(nu[0], nu[1])) # (rad) – True anomaly.
+def mean_z_enu(sigmas, Wm):
+    # transforms points to east, north, up; takes cartesian mean of enu; transforms enu back to aer
+    enu_mean = np.average(np.array([pm.aer2enu(*sigma, deg=False) for sigma in sigmas]), axis=0, weights=Wm)
+    return np.array(pm.enu2aer(*enu_mean, deg=False))
+
+
+@njit
+def mean_z_uvw(sigmas, Wm):
+    # transforms points to east, north, up; takes cartesian mean of enu; transforms enu back to aer
+    aers = np.empty(shape=sigmas.shape)
+    for i in range(len(aers)):
+        aers[i] = aer2uvw(sigmas[i])
+    uvw_mean = np.dot(Wm, aers)
+    return uvw2aer(uvw_mean)
+
+
+def init_state_vec(orbits='Default', random_state=np.random.RandomState()):
+    if orbits == 'Default':
+        orbits = ['LEO', 'MEO', 'GEO', 'LEO', 'MEO', 'GEO', 'Tundra', 'Molniya']
+    random_state.shuffle(orbits)
+    exo_atmospheric = False
+
+    inc = np.radians(random_state.uniform(0, 180)) # (rad) – Inclination
+    raan = np.radians(random_state.uniform(0, 360)) # (rad) – Right ascension of the ascending node.
+    argp = np.radians(random_state.uniform(low=0, high=360)) # (rad) – Argument of the pericenter.
+    nu = np.radians(random_state.uniform(0, 360)) # (rad) – True anomaly.
+
+    if orbits[-1] == 'LEO':
+        while not exo_atmospheric:
+            a = random_state.uniform(RE_eq+300*1000, RE_eq+2000*1000) # (m) – Semi-major axis.
+            ecc = random_state.uniform(0, .25) # (Unit-less) – Eccentricity.
+            b = a*np.sqrt(1-ecc**2)
+            if b > RE_eq+300*1000:
+                exo_atmospheric = True
+    if orbits[-1] == 'MEO':
+        while not exo_atmospheric:
+            a = random_state.uniform(RE_eq+2000*1000, RE_eq+35786*1000) # (m) – Semi-major axis.
+            ecc = random_state.uniform(0, .25) # (Unit-less) – Eccentricity.
+            b = a*np.sqrt(1-ecc**2)
+            if b > RE_eq+300*1000:
+                exo_atmospheric = True
+    if orbits[-1] == 'GEO':
+        a = 42164*1000 # (m) – Semi-major axis.
+        stationary = random_state.randint(0, 2)
+        ecc = stationary*random_state.uniform(0, .25) # (Unit-less) – Eccentricity.
+        inc = stationary*random_state.uniform(0, np.radians(0)) # (Unit-less) – Eccentricity.
+    if orbits[-1] == 'Molniya':
+        a = 26600*1000 # (m) – Semi-major axis.
+        inc = np.radians(63.4) # (rad) – Inclination
+        ecc = 0.737 # (Unit-less) – Eccentricity.
+        argp = np.radians(270) # (rad) – Argument of the pericenter.
+    if orbits[-1] == 'Tundra':
+        a = 42164*1000 # (m) – Semi-major axis.
+        inc = np.radians(63.4) # (rad) – Inclination
+        ecc = 0.2 # (Unit-less) – Eccentricity.
+        argp = np.radians(270) # (rad) – Argument of the pericenter.
+
     p = a*(1-ecc**2) # (km) - Semi-latus rectum or parameter
     return np.concatenate(coe2rv(k, p, ecc, inc, raan, argp, nu))
+
+
+def robust_cholesky(a):
+    try:
+        return cholesky(a)
+    except:
+        i = -6
+        done = False
+        while not done:
+            e = np.eye(len(a))*10**i
+            try:
+                return cholesky(a+e)
+            except:
+                i += 1
+            if i == 10:
+                done = True
+    raise np.linalg.LinAlgError
+
