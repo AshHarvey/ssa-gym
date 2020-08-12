a77ac82bea8f76dcfc9d8cc18435448fea569201
Ash <8992281+AshHarvey@users.noreply.github.com>
2020-08-12 02:49:48 -0400

    Update run_environment.py
    Changed env config loading & localized prop func to enable RLLib compatibility

--- run_environment.py
+++ run_environment.py
@@ -2,26 +2,25 @@ import gym
 import numpy as np
 from tqdm import tqdm
 from datetime import datetime
-from envs.dynamics import fx_xyz_farnocchia, fx_xyz_markley, fx_xyz_cowell
+from envs.dynamics import fx_xyz_farnocchia as fx, hx_aer_erfa as hx, mean_z_uvw as mean_z, residual_z_aer as residual_z
+from envs.dynamics import hx_xyz, mean_xyz, robust_cholesky
 from agents import agent_naive_greedy, agent_naive_random, agent_visible_greedy, agent_visible_random, agent_pos_error_greedy
 from envs.transformations import arcsec2rad
 
-P_0 = np.diag((1000**2, 1000**2, 1000**2, 10**2, 10**2, 10**2))
+from envs.farnocchia import fx_xyz_farnocchia as fx
 
-R = np.diag((1*arcsec2rad**2, 1*arcsec2rad**2, 1000**2))
+sample_orbits = np.load('/home/ash/PycharmProjects/ssa-gym/envs/1.5_hour_viz_20000_of_20000_sample_orbits_seed_0.npy')
 
-x_sigma = (0, 0, 0, 0, 0, 0) # (1000, 1000, 1000, 10, 10, 10)
-z_sigma = (0, 0, 0) # (1, 1, 1000)
+config = {'steps': 480, 'rso_count': 10, 'time_step': 30., 't_0': datetime(2020, 5, 4, 0, 0, 0), 'obs_limit': 15,
+          'observer': (38.828198, -77.305352, 20.0), 'update_interval': 1, 'obs_type': 'aer',  'z_sigma': (1, 1, 1e3),
+          'x_sigma': tuple([1e5]*3+[1e2]*3), 'q_sigma': 0.000025, 'P_0': np.diag(([1e5**2]*3 + [1e2**2]*3)),
+          'R': np.diag(([arcsec2rad**2]*2 + [1e3**2])), 'alpha': 0.0001, 'beta': 2., 'kappa': 3-6, 'fx': fx, 'hx': hx,
+          'mean_z': mean_z, 'residual_z': residual_z, 'msqrt': robust_cholesky, 'orbits': sample_orbits}
 
-kwargs = {'steps': 2880, 'rso_count': 50, 'time_step': 30.0, 't_0': datetime(2020, 5, 4, 0, 0, 0),
-          'obs_limit': 15, 'observer': (38.828198, -77.305352, 20.0), 'x_sigma': x_sigma,
-          'z_sigma': z_sigma, 'q_sigma': 0.001, 'P_0': P_0, 'R': R, 'update_interval': 1,
-          'orbits': np.load('envs/1.5_hour_viz_20000_of_20000_sample_orbits_seed_0.npy'), 'fx': fx_xyz_farnocchia}
-
-env = gym.make('ssa_tasker_simple-v2', **kwargs)
+env = gym.make('ssa_tasker_simple-v2', **{'config': config})
 env.seed(0)
 obs = env.reset()
-agent = [agent_pos_error_greedy, agent_naive_random, agent_visible_random, agent_naive_greedy, agent_visible_greedy][0]
+agent = agent_visible_greedy
 
 done = False
 for i in tqdm(range(env.n)):
